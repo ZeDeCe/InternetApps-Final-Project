1b7ac4d1ba3ca309d5c529be04090fd3648aceba
ZeDeCe <no9no3@gmail.com>
2024-08-31 16:05:09 +0300

    add: userpage requires login

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
-  res.render('index.ejs', {username: req.username, isadmin: req.isadmin});
+  res.render('index.ejs', {username: req.session.username, isadmin: req.session.isAdmin});
 });
 
 module.exports = router;
