37c669c2079910a320a930847bf89d3e7d6449b1
shaqedmov <shaqedmov@gmail.com>
2024-08-31 19:04:52 +0300

    add: static pages (policies)

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,20 @@
 var express = require('express');
 var router = express.Router();
 
-router.get('/', function(req, res, next) {
+router.get('/', function(req, res) {
   res.render('index.ejs', {username: req.session.username, isadmin: req.session.isAdmin});
 });
+
+router.get('/policies', function(req, res) {
+  switch (req.query.page){
+    case "shipping_info":
+      res.render('shipping_info.ejs')
+    case "return_policy":
+      res.render('return_policy.ejs')
+    case "privacy_policy":
+      res.render('privacy_policy.ejs')
+  }
+
+})
 
 module.exports = router;
