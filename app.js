596b7ab75555ea6ffbd57037d668e90482657a7d
shaqedmov <shaqedmov@gmail.com>
2024-09-07 14:00:29 +0300

    fix: include css correctly

--- app.js
+++ app.js
@@ -26,6 +26,7 @@ app.use(session({
 app.set("view engine", "ejs")
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }))
+app.use(express.static(__dirname + '/public'))
 
 app.use('/', indexRouter);
 app.use('/login', loginRouter)
