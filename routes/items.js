9fce0fc318fef8789b3c092427c2703b97371bf8
ZeDeCe <86704700+ZeDeCe@users.noreply.github.com>
2024-09-12 15:09:55 +0300

    Revert "main-page"

--- routes/items.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const itemController = require('../controllers/item');
-
-router.route("/").get(itemController.getItems);
-router.get('/search', itemController.searchItems);
-
-module.exports = router;
