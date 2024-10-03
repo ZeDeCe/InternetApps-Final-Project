4adb85d932e95d2d5a8d47c97e56b1e2a96ceb6f
ZeDeCe <no9no3@gmail.com>
2024-10-04 02:48:15 +0300

    refactor: admins can edit themselves but not delete

--- controllers/user.js
+++ controllers/user.js
@@ -2,19 +2,22 @@ const userService = require("../services/user")
 
 async function deleteUser(req, res) {
     var username = req.body.username
-    if(await isAdmin(username)) {
-        return res.send({"user" : {"message": "Cannot delete an admin"}})
+    if(await userService.isAdmin(username)) {
+        return res.status(500).send("Attempting to delete admin")
     }
     if(await userService.deleteUser(req.body.username)) {
-        res.status(404).send("Failed deleting user")
+        return res.status(500).send("Failed deleting user")
     }
-    res.send()
+    return res.send()
 }
 
 async function updateUser(req, res) {
     var username = req.body.username
-    if(username != req.session.username && await isAdmin(username)) {
+    if(username !== req.session.username && await userService.isAdmin(username)) {
         return res.send({"user" : {"message": "Cannot edit another admin"}})
+    }
+    if(username === req.session.username && !req.body.data.isAdmin) {
+        return res.send({"isAdmin" : {"message": "Cannot remove yourself from admin list"}})
     }
     res.send(await userService.updateUser(username, req.body.data))
 }
@@ -36,7 +39,7 @@ async function getUser(req, res, next) {
 }
 
 function isAdmin(req, res, next) {
-    if (req.session.isAdmin) {
+    if (req && req.session && req.session.isAdmin) {
         return next()
     }
     else {
