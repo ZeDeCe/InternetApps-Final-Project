661f4898d5abe6ac7132c2f80e4b3abb2912c342
ZeDeCe <no9no3@gmail.com>
2024-10-04 01:48:37 +0300

    fix: control flow in admin usercontrol with delete and update

--- controllers/user.js
+++ controllers/user.js
@@ -1,20 +1,26 @@
 const userService = require("../services/user")
 
 async function deleteUser(req, res) {
-    res.send(await userService.deleteUser(req.body.username))
+    var username = req.body.username
+    if(await isAdmin(username)) {
+        return res.send({"user" : {"message": "Cannot delete an admin"}})
+    }
+    if(await userService.deleteUser(req.body.username)) {
+        res.status(404).send("Failed deleting user")
+    }
+    res.send()
 }
 
 async function updateUser(req, res) {
-    res.send(await userService.updateUser(req.body.username, req.body.data))
+    var username = req.body.username
+    if(username != req.session.username && await isAdmin(username)) {
+        return res.send({"user" : {"message": "Cannot edit another admin"}})
+    }
+    res.send(await userService.updateUser(username, req.body.data))
 }
 
 async function createUserAsAdmin(req, res, next) {
-    var err = await userService.createUser(req.body.user)
-    if (err) {
-        res.status(404).send(err)
-    } else {
-        return next()
-    }
+    res.send(await userService.createUser(req.body.user))
 }
 
 async function getUser(req, res, next) {
@@ -41,8 +47,6 @@ function isAdmin(req, res, next) {
 async function validateUsername(req, res) {
     res.send(await userService.validateUsername(req.body.username))
 }
-
-
 
 module.exports = {
     deleteUser,
