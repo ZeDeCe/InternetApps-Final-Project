ea7aa6142e5905116b08cf38a30d2ae083a87083
ZeDeCe <no9no3@gmail.com>
2024-10-08 00:09:54 +0300

    fix: admin has it's own routes for /all_orders

--- routes/user_page.js
+++ routes/user_page.js
@@ -4,11 +4,11 @@ const userpageController = require('../controllers/user_page')
 const loginController = require("../controllers/login")
 const userController = require("../controllers/user")
 
-router.route("/all_orders").get(loginController.isLoggedIn, userpageController.getOrdersPage)
-
-// Admin cannot get to userpage or personalpage
+// Admin cannot get to userpage
 router.use(userController.isNotAdmin)
+
 router.route("/").get(loginController.isLoggedIn, userpageController.getUserPage)
+router.route("/all_orders").get(loginController.isLoggedIn, userpageController.getOrdersPage)
 router.route("/userpersonaldata").get(loginController.isLoggedIn, userpageController.getPersonalPage)
 
 module.exports = router;
